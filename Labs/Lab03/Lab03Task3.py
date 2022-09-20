3e6292d39a7409a8cc203ac782671cce0693b659
Abdullah Alabbas <SEabdullahT@gmail.com>
2022-09-20 15:22:13 +0300

    .

--- Labs/Lab03/Lab03Task3.py
+++ Labs/Lab03/Lab03Task3.py
@@ -1,4 +1,4 @@
 firstName = input("What is your first name: ").lower() # i can use the casefold() function instead of lower()
 dateOfBirth = int(input("What is your date of birth: "))
 newDateOfBirth = dateOfBirth * 2
-print(firstName + firstName[-1].upper() + firstName[0].upper() + "$" + str(newDateOfBirth))
+password = print(firstName + firstName[-1].upper() + firstName[0].upper() + "$" + str(newDateOfBirth))
