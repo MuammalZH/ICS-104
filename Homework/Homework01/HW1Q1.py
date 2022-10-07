f7e6379ed30426fd0fbc19eee3f4e497ffb08fc2
Abdullah Alabbas <SEabdullahT@gmail.com>
2022-10-08 00:57:52 +0300

    .

--- Homework/Homework01/HW1Q1.py
+++ Homework/Homework01/HW1Q1.py
@@ -1,18 +1,63 @@
 # Question 1: Logic Functions Calculator
-# I will use the camelCase
-logicName = input("Enter logic function name (and, or, not): ").lower().strip() # i used the lower and strip methods to ignore the user errors
-firstValue = int(input("Enter value of condition A: ")) # This is the value of condition A
-secondValue = int(input("Enter value of condition A: ")) # This is the value of condition B
+# I will use the camelCase.
+logicName = input("Enter logic function name (and, or, not): ").lower() # i used the lower method to ignore the user errors
 
-# The second step is to validate the function names.
+# This is to validate the function names.
 if logicName == "and" :
-    pass
+
+    firstValue = int(input("Enter value of condition A: ")) # This is the value of condition A
+    secondValue = int(input("Enter value of condition B: ")) # This is the value of condition B
+
+    # This is to validate against the user input.
+    if firstValue == 0:
+        if secondValue == 0: # This is the first condition
+            print('"and" logic function result = 0')
+        elif secondValue == 1: # This is the second condition
+            print('"and" logic function result = 0')   
+        else:
+            print("Error: either B is not a digit, has more than one digit, or is out of the 0,1 range")
+
+    elif firstValue == 1:
+        if secondValue == 0: # This is the first condition
+            print('"and" logic function result = 0')
+        elif secondValue == 1: # This is the second condition
+            print('"and" logic function result = 1')
+        else:
+            print("Error: either B is not a digit, has more than one digit, or is out of the 0,1 range")
+    else:
+        print("Error: either A is not a digit, has more than one digit, or is out of the 0,1 range")
 
 elif logicName == "or" :
-    pass
+    firstValue = int(input("Enter value of condition A: ")) # This is the value of condition A
+    secondValue = int(input("Enter value of condition B: ")) # This is the value of condition B
+    
+    if firstValue == 0:
+        if secondValue == 0: # This is the first condition
+            print('"or" logic function result = 0')
+        elif secondValue == 1: # This is the second condition
+            print('"or" logic function result = 1')
+        else:
+            print("Error: either B is not a digit, has more than one digit, or is out of the 0,1 range")
+    
+    elif firstValue == 1:
+        if secondValue == 0: # This is the first condition
+            print('"or" logic function result = 1')
+        elif secondValue == 1: # This is the second condition
+            print('"or" logic function result = 1')
+        else:
+            print("Error: either B is not a digit, has more than one digit, or is out of the 0,1 range")
+    else:
+        print("Error: either A is not a digit, has more than one digit, or is out of the 0,1 range")
 
 elif logicName == "not" :
-    pass
 
-else:
+    firstValue = int(input("Enter value of condition A: ")) # This is the value of condition A
+
+    if firstValue == 0: # This is the first condition
+        print('"not" logic function result = 1')
+    elif firstValue == 1: # This is the second condition
+        print('"not" logic function result = 0')
+    else:
+        print("Error: either A is not a digit, has more than one digit, or is out of the 0,1 range")
+else: # to validate against function names
     print("Error: unknown logic function")
---
+++ Homework/Homework01/HW1Q2.py
@@ -0,0 +1,37 @@
+# Question 2: Measuring Distance using 3 p-norm Implementations.
+from math import sqrt # for line 21 or i can use **(1/2)
+# i will use the camelCase
+normName = input("Enter desired norm name (infinity, 1-norm, 2-norm): ").lower() # i used the lower method to ignore the user errors
+
+if normName == "1-norm": # i will use the abs function from chapter two
+    x1 = float(input("Enter value of coordinate value x1: "))
+    y1 = float(input("Enter value of coordinate value y1: "))
+    x2 = float(input("Enter value of coordinate value x2: ")) 
+    y2 = float(input("Enter value of coordinate value y2: "))
+
+    oneNorm = abs(x1 - x2) + abs(y1 - y2) # i can put the numpers in one abs but i think this way is cleaner
+    print("1-norm distance value =" , oneNorm) # i can print it without define the oneNorm function but ithink this way is cleaner
+
+elif normName == "2-norm":
+    x1 = float(input("Enter value of coordinate value x1: "))
+    y1 = float(input("Enter value of coordinate value y1: "))
+    x2 = float(input("Enter value of coordinate value x2: "))
+    y2 = float(input("Enter value of coordinate value y2: "))
+
+    twoNorm = sqrt((x1 - x2)**2 + (y1 - y2)**2)  # sqrt function from chapter two
+    print("1-norm distance value =" , twoNorm)  # i can print it without define the twoNorm function but ithink this way is cleaner
+
+elif normName == "infinity": # i waii use the max function from chapter two
+    x1 = float(input("Enter value of coordinate value x1: "))
+    y1 = float(input("Enter value of coordinate value y1: "))
+    x2 = float(input("Enter value of coordinate value x2: "))
+    y2 = float(input("Enter value of coordinate value y2: "))
+
+    x = abs(x1 - x2)
+    y = abs(y1 - y2)
+    infinityNorm = max(x , y)
+    print("infinity norm distance value = " , infinityNorm) 
+
+
+else:
+    print("Error: unknown norm")
