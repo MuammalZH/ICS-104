323a3856ff6319ea5d197e3cdeddd7d3918e3db9
Abdullah Alabbas <SEabdullahT@gmail.com>
2022-10-11 12:31:39 +0300

    :)

--- Labs/Lab06/Lab06Task1.py
+++ Labs/Lab06/Lab06Task1.py
@@ -1,17 +1,3 @@
-# using while loop, write a program that reads from the user the value of an integer number n 
-# (has to be positive and multiple of 5). Then your program finds and displays the sum of multiples of 5 from 5 to n i.e.
-# sum=5+10+15+20+……..+n
-# If the user enters wrong input, your program displays “Wrong input” message.
-# Sample runs are shown below:
-# Enter n ( >0 and muliple of 5 ): -10
-# Wrong input
-
-# Enter n ( >0 and muliple of 5 ): 11
-# Wrong input
-
-# Enter n ( >0 and muliple of 5 ): 25
-# Sum = 75
- # 15 == 5 + 10 + 15
 n = int(input("Enter n ( >0 and muliple of 5 ): "))
 sum = 0
 i = 5
--- Labs/Lab06/Lab06Task4.py
+++ Labs/Lab06/Lab06Task4.py
@@ -1,16 +1,3 @@
-# Write a program that prompts for and reads the number  𝑛  >=3 . If  𝑛<3  
-# your program must display an error message and terminate; otherwise, using for loop,
-# it prints all numbers between 1 and n that are divisible by either 3 or 5 on the same line:
-
-# The following are sample runs of the program.
-
-# Enter n ( >=3 ): 2
-# Wrong input n must be >= 3
-
-# Enter n ( >=3 ): 27
-# numbers between 1 and 27 divisible by 3 or 5 are:
-# 3 5 6 9 10 12 15 18 20 21 24 25 27
-
 n = int(input("Enter n ( >=3 ): "))
 sum = 0
 if n >= 3:
