adc6d1016fdeaa405a3c7c793f0d3a085a2667b8
Abdullah Alabbas <SEabdullahT@gmail.com>
2022-10-13 14:54:48 +0300

    :)

--- Labs/Lab06/Lab06Task1.py
+++ Labs/Lab06/Lab06Task1.py
@@ -3,9 +3,9 @@ sum = 0
 i = 5
 if n >= 0 and n % 5 == 0:
     while i <= n:
-        sum += i
+        sum = sum + i
         i = i +5        
-    print(sum)        
+        print(sum)        
 else:
     print("Wrong input")
     
--- Labs/Lab06/Lab06Task2.py
+++ Labs/Lab06/Lab06Task2.py
@@ -1,12 +1,15 @@
 evenNum = 0
 oddNum = 0
 num = input("Enter a positive number: (or press Enter key to finish: )")
-while num !="":
-    value = int(num)
-    if value % 2 == 0:
-        evenNum +=1
-    else:
-        oddNum +=1
-    num = input("Enter a positive number: (or press Enter key to finish: )")
-print(f"You entered {oddNum} odd and {evenNum} even numbers")
-        
+if num != "":
+    while num !="":
+        value = int(num)
+        if value % 2 == 0:
+            evenNum +=1
+        else:
+            oddNum +=1
+        num = input("Enter a positive number: (or press Enter key to finish: )")
+    print(f"You entered {oddNum} odd and {evenNum} even numbers")
+
+else:
+    print("No positive number was entered.")        
