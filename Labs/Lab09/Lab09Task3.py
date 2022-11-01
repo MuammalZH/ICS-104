7ef848a0d769c095cd308b18ebe325102b6a9ec8
Abdullah Alabbas <SEabdullahT@gmail.com>
2022-11-01 11:21:33 +0300

    :)

--- Labs/Lab09/Lab09Task3.py
+++ Labs/Lab09/Lab09Task3.py
@@ -21,3 +21,34 @@ def intValue(n):
 
 if __name__ =="__main__": # This line is optinal  مو معانا في المنهج
     main()
+    
+    
+    
+    
+    
+    # طريقة ثانية
+    
+#     def main():
+#     x = int(input("Enter a positive integer value: "))
+#     factors = intValue(x) 
+#     if factors == False:
+#         print("%d is a prime number"% x)
+#     else:
+#         print("The factors of %d exculding 1 and %d are:"% (x,x))
+#         print(factors)
+    
+# def intValue(n):
+#     lst = [] 
+#     for i in range(2,n):
+#         if n % i == 0:      
+#             lst.append(i)
+#     if len(lst) == 0:
+#         return False
+#     else:
+#         return lst
+    
+    
+    
+
+# if __name__ =="__main__": # This line is optinal  مو معانا في المنهج
+#     main()
